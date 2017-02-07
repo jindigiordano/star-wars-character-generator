2849e8c1f8ef38588a9fb3a08c07322ae9414ff1
Jin Di Giordano <jin.di.giordano@gmail.com>
2017-02-07 15:13:16 -0800

    Fix JS errors to display species and homeworld details

--- app/assets/javascripts/characters.js
+++ app/assets/javascripts/characters.js
@@ -1,13 +1,10 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
-
 $(document).on("turbolinks:load", function() {
 
 
   $('.show-details').on('click', function(event){
       event.preventDefault();
+      console.log("poop");
       $(event.target).closest('tr').find('.attribute-details').toggle();
   });
-  
-)};
+
+});
