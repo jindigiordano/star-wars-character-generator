bcaaa2a0b2cb225db89dd0be8144348d6a85862d
Jin Di Giordano <jin.di.giordano@gmail.com>
2017-02-07 15:31:50 -0800

    Refactor character controller and move logic and api calls to helper

--- app/assets/javascripts/characters.js
+++ app/assets/javascripts/characters.js
@@ -3,7 +3,6 @@ $(document).on("turbolinks:load", function() {
 
   $('.show-details').on('click', function(event){
       event.preventDefault();
-      console.log("poop");
       $(event.target).closest('tr').find('.attribute-details').toggle();
   });
 
